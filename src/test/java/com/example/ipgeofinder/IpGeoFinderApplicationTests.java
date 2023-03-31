cfcd95992dc8fef44ac7f5af37ddd750764c2a11
Mario Murillo <mario.murillo@travelx.io>
2023-03-31 13:42:59 -0500

    Feat: add project scheleton

---
+++ src/test/java/com/example/ipgeofinder/IpGeoFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipgeofinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpGeoFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
